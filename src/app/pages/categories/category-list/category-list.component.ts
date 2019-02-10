8e4d57fe06161b4d428e91771ce2b61f58985e9f
jeeeanlefchak <jean_2103@hotmail.com>
2019-02-10 13:53:13 -0200

    fgfgs

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  
+  categories : Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService:CategoryService) {
+
+   }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(categories =>{
+      this.categories = categories;
+    })
   }
 
 }
