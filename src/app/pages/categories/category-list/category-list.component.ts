ef374606a884362232940413185d71bb820b0565
jeeeanlefchak <jean_2103@hotmail.com>
2019-02-11 13:05:45 -0200

    K

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoryService } from '../shared/category.service';
-import { Category } from '../shared/category.model';
+import { Component, OnInit } from "@angular/core";
+import { CategoryService } from "../shared/category.service";
+import { Category } from "../shared/category.model";
+import { element } from "@angular/core/src/render3";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"]
 })
 export class CategoryListComponent implements OnInit {
-  
-  categories : Category[] = [];
+  categories: Category[] = [];
 
-  constructor(private categoryService:CategoryService) {
-
-   }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
-    this.categoryService.getAll().subscribe(categories =>{
+    this.categoryService.getAll().subscribe(categories => {
       this.categories = categories;
-    })
+    });
   }
 
+  deleteCategory(category) {
+    const mustDelete = confirm("Deseja realemnte excluir este item ?");
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(() => {
+        this.categories = this.categories.filter(
+          element => element != category
+        );
+        () => alert("Erro ao tentar Excluir !");
+      });
+    }
+  }
 }
