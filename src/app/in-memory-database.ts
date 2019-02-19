737fad08d6a68574b08806ab37306edfe6508e23
jeeeanlefchak <jean_2103@hotmail.com>
2019-02-19 12:58:29 -0300

    l

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,11 +1,17 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 import { Category } from './pages/categories/shared/category.model'
+import { Entry } from './pages/enteris/shared/entry.model';
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
         const categories : Category[] = [
             {id:1, name: 'Lazer', description:''}
         ];
 
-        return {categories};
+        const entries : Entry[] = [
+            { id:1, name: 'Gás de cozinha', categoryId : categories[0].id, category: categories[0], paid: true , date:'19/02/2019', amount:"70,00", type:'expense', description:"" },
+            { id:1, name: 'Gás de cozinha', categoryId : categories[0].id, category: categories[0], paid: true , date:'19/02/2019', amount:"70,00", type:'expense', description:"" }
+        ]
+
+        return {categories, entries};
     }
 }
