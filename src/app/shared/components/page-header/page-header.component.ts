5faf56635b64379491effba16de94e88d0f1f7c6
jeeeanlefchak <jean_2103@hotmail.com>
2019-03-21 20:43:28 -0300

    editando a tela de reports

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -10,6 +10,7 @@ export class PageHeaderComponent implements OnInit {
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
+  @Input('show-button') showButton: boolean = true;
 
   constructor() { }
 
